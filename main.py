da56c997b86c42b7acd68bca5ad1c5b1d4e203a1
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-05-09 00:27:28 +0530

    main window

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import tkinter as tk
 from tkinter import font
 
+# Create the main window
+win = tk.Tk()
+win.title("⏳ Auto-Clear Typing Box")
+win.geometry("500x300")
+win.configure(bg="#2c3e50")  # Dark background
+
 
